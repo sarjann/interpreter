3973baded24178f001dc5d27b0f924a45b331baa
Sarjan Narwan <imsar@gmail.com>
2023-11-30 19:37:06 +0000

    Update lexer
    Lexer seems to be fully working now

--- lexer/src/lib.rs
+++ lexer/src/lib.rs
@@ -1,5 +1,4 @@
 use std::fmt::{Display, Formatter};
-
 pub enum Token {
     Illegal,
     Eof,
@@ -11,19 +10,32 @@ pub enum Token {
     // Operators
     Assign,
     Plus,
+    Minus,
+    Bang,
+    Asterisk,
+    Eq,
+    NotEq,
 
     // Delimiters
     Comma,
     Semicolon,
+    Slash,
 
     LParen,
     RParen,
     LBrace,
     RBrace,
+    LT,
+    RT,
 
     // Keywords
     Function,
     Let,
+    True,
+    False,
+    If,
+    Else,
+    Return,
 }
 
 impl Display for Token {
@@ -35,14 +47,29 @@ impl Display for Token {
             Token::Int(i) => write!(f, "Token::Int({i})"),
             Token::Assign => write!(f, "Token::Assign"),
             Token::Plus => write!(f, "Token::Plus"),
+            Token::Minus => write!(f, "Token::Minus"),
+            Token::Bang => write!(f, "Token::Bang"),
+            Token::Asterisk => write!(f, "Token::Asterisk"),
+            Token::Eq => write!(f, "Token::Eq"),
+            Token::NotEq => write!(f, "Token::NotEq"),
             Token::Comma => write!(f, "Token::Comma"),
             Token::Semicolon => write!(f, "Token::Semicolon"),
+            Token::Slash => write!(f, "Token::Slash"),
             Token::LParen => write!(f, "Token::LParen"),
             Token::RParen => write!(f, "Token::RParen"),
             Token::LBrace => write!(f, "Token::LBrace"),
             Token::RBrace => write!(f, "Token::RBrace"),
+            Token::LT => write!(f, "Token::LT"),
+            Token::RT => write!(f, "Token::RT"),
+
+            // Keywords
             Token::Function => write!(f, "Token::Function"),
             Token::Let => write!(f, "Token::Let"),
+            Token::True => write!(f, "Token::True"),
+            Token::False => write!(f, "Token::False"),
+            Token::If => write!(f, "Token::If"),
+            Token::Else => write!(f, "Token::Else"),
+            Token::Return => write!(f, "Token::Return"),
         }
     }
 }
@@ -66,31 +93,101 @@ impl Lexer {
         lexer
     }
     pub fn next_token(&mut self) -> Token {
+        self.skip_whitespace();
         let ch = self.ch.expect("Error: ch is None");
         let token = match ch {
             b'a'..=b'z' | b'A'..=b'Z' => self.read_identifier(ch),
-            b'=' => Token::Assign,
+            b'0'..=b'9' => self.read_number(ch),
+            b'=' => match self.look_ahead_one() {
+                Some(b'=') => {
+                    self.read_char();
+                    return Token::Eq;
+                }
+                _ => Token::Assign,
+            },
             b'+' => Token::Plus,
+            b'-' => Token::Minus,
+            b'!' => match self.look_ahead_one() {
+                Some(b'=') => {
+                    self.read_char();
+                    return Token::NotEq;
+                }
+                _ => Token::Bang,
+            },
+            b'*' => Token::Asterisk,
             b',' => Token::Comma,
             b';' => Token::Semicolon,
+            b'/' => Token::Slash,
             b'(' => Token::LParen,
             b')' => Token::RParen,
             b'{' => Token::LBrace,
             b'}' => Token::RBrace,
+            b'<' => Token::LT,
+            b'>' => Token::RT,
             0 => Token::Eof,
-            _ => Token::Illegal,
+            _ => {
+                println!("Unknown character: {}", char::from(ch));
+                Token::Illegal
+            }
         };
         self.read_char();
         token
     }
 
+    pub fn look_ahead_one(&self) -> Option<u8> {
+        let look_ahead_pos = self.read_pos + 1;
+        if look_ahead_pos >= self.input.len() {
+            None
+        } else {
+            Some(self.input[look_ahead_pos])
+        }
+    }
+
+    pub fn skip_whitespace(&mut self) {
+        loop {
+            match self.ch.expect("No char found") {
+                b' ' | b'\n' | b'\r' | b'\t' => self.read_char(),
+                _ => break,
+            }
+        }
+    }
+
     pub fn get_keyword_token(s: &str) -> Option<Token> {
         let token = match s {
-            "let" => Token::Let,
             "fn" => Token::Function,
+            "let" => Token::Let,
+            "true" => Token::True,
+            "false" => Token::False,
+            "if" => Token::If,
+            "else" => Token::Else,
+            "return" => Token::Return,
             _ => return None,
         };
         Some(token)
+    }
+
+    pub fn read_number(&mut self, ch: u8) -> Token {
+        let mut ch = ch;
+        let mut us: Vec<u8> = vec![];
+        loop {
+            match ch {
+                b'0'..=b'9' => {
+                    us.push(ch);
+                    self.read_char();
+                    ch = self.ch.expect("Missing ch (int)");
+                }
+                _ => break,
+            }
+        }
+
+        let mut number = 0;
+        for c in us {
+            number = number * 10 + (c - b'0') as i32;
+        }
+
+        self.pos -= 1;
+        self.read_pos -= 1;
+        Token::Int(number)
     }
 
     pub fn read_identifier(&mut self, ch: u8) -> Token {
@@ -101,7 +198,7 @@ impl Lexer {
                 b'a'..=b'z' | b'A'..=b'Z' | b'_' => {
                     us.push(ch);
                     self.read_char();
-                    ch = self.ch.expect("Missing ch");
+                    ch = self.ch.expect("Missing ch (ident)");
                 }
                 _ => break,
             }
@@ -113,6 +210,9 @@ impl Lexer {
             Some(x) => x,
             _ => Token::Ident(s),
         };
+        self.pos -= 1;
+        self.read_pos -= 1;
+
         token
     }
 
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,21 @@
 use lexer::{Lexer, Token};
-// use ast::AST;
-// use std::fs;
+use ast::AST;
+use std::fs;
 
 fn main() {
-    // let source_file = "file.lang";
-    // let input_string = fs::read_to_string(source_file).expect(
-    //     "Issue loading\
-    //     input string",
-    // );
-    let input_string = "let x = 5 + 5;".to_string();
+    let source_file = "file.lang";
+    let input_string = fs::read_to_string(source_file).expect(
+        "Issue loading\
+        input string",
+    );
+    // let input_string = "let x = 5 + 5;".to_string();
     let input_bytes = input_string.into_bytes();
 
 
     let mut lexer = Lexer::new(input_bytes);
     loop {
         let token = lexer.next_token();
-        println!("{}", token);
+        print!("{}  ", token);
         match token {
             Token::Eof => break,
             _ => (),
